933aab5b85415ae52adfc0587045a84f74fd54d1
s17737 <s17737@pjwstk.edu.pl>
2020-04-02 18:41:09 +0200

    cwiczenia nr 3

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+   
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            foreach(Student s in _dbService.GetStudents())
+            {
+                if(s.IdStudent == id)
+                {
+                    return Ok(s);
+                }
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            foreach (Student s in _dbService.GetStudents())
+            {
+                if (s.IdStudent == id)
+                {
+                    return Ok(s);
+                }
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+    }
+}
